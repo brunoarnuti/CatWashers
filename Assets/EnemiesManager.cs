b0f2c53f38ba63a12ce63fb2d73a2e7d6d4ba251
vanearevi92 <vanearevi92@gmail.com>
2023-08-19 14:00:08 -0300

    starting enemies

---
+++ Assets/EnemiesManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesManager : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] Vector2 spawnArea;
+    [SerializeField] float spawnTimer;
+    float timer;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0f )
+        {
+            spawnEnemy();
+            timer = spawnTimer;
+        }
+    }
+
+    private void spawnEnemy()
+    {
+        Vector3 position = new Vector3(
+            UnityEngine.Random.Range(-spawnArea.x, spawnArea.x),
+            UnityEngine.Random.Range(-spawnArea.y, spawnArea.y),
+            0f);
+
+        GameObject newEnemy = Instantiate(enemy);
+        newEnemy.transform.position = position;
+    }
+}
