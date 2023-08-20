c35bc1d687b90c64874d9281a17f2bed2adf96c8
RainogReal <142637220+RainogReal@users.noreply.github.com>
2023-08-19 21:59:10 -0300

    Animaciones
    Personaje y arma

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     private Vector3 movementVector;
 
+    [Header("Animation")]
+    private Animator animator;
+
     [SerializeField] private float speed = 3f;
+
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
     }
 
@@ -48,6 +53,8 @@ public class PlayerController : MonoBehaviour
         movementVector *= speed;
 
         _rigidbody2D.velocity = movementVector;
+
+        animator.SetFloat("Horizontal", Mathf.Abs(movementVector.x));
     }
 
     private void  OnCollisionEnter2D(Collision2D collision)
